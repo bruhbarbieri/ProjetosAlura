1326f7a7ad85a0df8997391d9914d755e1b0c1e5
bruna.barbieri <bruna.barbieri@db1.com.br>
2024-08-21 13:45:56 -0300

    dados temporada

--- Formação Java Web/screenmatch/screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ Formação Java Web/screenmatch/screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,9 +1,12 @@
 package br.com.alura.screenmatch.principal;
 
 import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
@@ -22,7 +25,13 @@ public class Principal {
         DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
         System.out.println(dados);
 
-        //https://www.omdbapi.com/?t=gilmore+girls&apikey=eceac216
-        //.replace("", "+")
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for(int i = 1; i<=dados.totalTemporadas(); i++) {
+			json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + API_KEY);
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+		temporadas.forEach(System.out::println);
     }
 }

551e4a6a21760fd741e9ca7c8d5a71b01a30fa01
Bruna Barbieri <bruh_barbieri@hotmail.com>
2024-08-26 11:17:01 -0300

    Revert "dados temporada"
    This reverts commit 1326f7a7ad85a0df8997391d9914d755e1b0c1e5.

--- Formação Java Web/screenmatch/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ Formação Java Web/screenmatch/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -16,11 +16,25 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+<<<<<<< HEAD
 		var consumoApi = new ConsumoApi();
 		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=eceac216");
 		System.out.println(json);
 		ConverteDados conversor = new ConverteDados();
 		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 		System.out.println(dados);
+=======
+		Principal principal = new Principal();
+		principal.exibeMenu();
+
+//		List<DadosTemporada> temporadas = new ArrayList<>();
+//
+//		for(int i = 1; i<=dados.totalTemporadas(); i++) {
+//			json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=6585022c");
+//			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+//			temporadas.add(dadosTemporada);
+//		}
+//		temporadas.forEach(System.out::println);
+>>>>>>> parent of 1326f7a (dados temporada)
 	}
 }

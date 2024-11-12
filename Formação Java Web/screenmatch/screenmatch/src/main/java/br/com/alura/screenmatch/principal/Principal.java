4a008d15287c3a09eb350139076d67f537083cb5
Bruna Barbieri <bruh_barbieri@hotmail.com>
2024-11-11 22:28:17 -0300

    atualizações

--- Formação Java Web/screenmatch/screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ Formação Java Web/screenmatch/screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -69,19 +69,25 @@ public class Principal {
 
         episodios.forEach(System.out::println);
 
-        System.out.println("Digite um trecho do título do episódio");
+//        System.out.println("Digite um trecho do título do episódio");
+//
+//        var trechoTitulo = leitura.nextLine();
+//
+//        Optional<Episodio> episodioBuscado = episodios.stream()
+//                .filter(e -> e.getTitulo().toUpperCase().contains(trechoTitulo.toUpperCase()))
+//                .findFirst();
+//        if(episodioBuscado.isPresent()){
+//            System.out.println("Episódio encontrado!");
+//            System.out.println("Temporada: " + episodioBuscado.get().getTemporada());
+//        } else {
+//            System.out.println("Episódio não encontrado!");
+//        }
 
-        var trechoTitulo = leitura.nextLine();
-
-        Optional<Episodio> episodioBuscado = episodios.stream()
-                .filter(e -> e.getTitulo().toUpperCase().contains(trechoTitulo.toUpperCase()))
-                .findFirst();
-        if(episodioBuscado.isPresent()){
-            System.out.println("Episódio encontrado!");
-            System.out.println("Temporada: " + episodioBuscado.get().getTemporada());
-        } else {
-            System.out.println("Episódio não encontrado!");
-        }
+        Map<Integer, Double> avaliacoesPorTemporada = episodios.stream()
+                .filter(e -> e.getAvaliacao() > 0.0)
+                .collect(Collectors.groupingBy(Episodio::getTemporada,
+                        Collectors.averagingDouble(Episodio::getAvaliacao)));
+        System.out.println(avaliacoesPorTemporada);
 //
 //        System.out.println("A partir de que ano você deseja ver os episódios? ");
 //        var ano = leitura.nextInt();

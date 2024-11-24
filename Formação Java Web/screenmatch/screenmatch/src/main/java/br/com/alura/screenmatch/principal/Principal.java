676798a445bf0f4412c2bb4a2141308fb1673d83
Bruna Barbieri <bruh_barbieri@hotmail.com>
2024-11-23 22:45:32 -0300

    atualizações

--- Formação Java Web/screenmatch/screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ Formação Java Web/screenmatch/screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -104,5 +104,14 @@ public class Principal {
 //                                " Episódio: " + e.getTitulo() +
 //                                " Data lançamento: " + e.getDataLancamento().format(formatador)
 //                ));
+
+        DoubleSummaryStatistics est = episodios.stream()
+                .filter(e -> e.getAvaliacao() > 0.0)
+                .collect(Collectors.summarizingDouble(Episodio::getAvaliacao));
+        System.out.println(est);
+        System.out.println("Média: " + est.getAverage());
+        System.out.println("Melhor episódio: " + est.getMax());
+        System.out.println("Pior episódio: " + est.getMin());
+        System.out.println("Quantidade: " + est.getCount());
     }
 }

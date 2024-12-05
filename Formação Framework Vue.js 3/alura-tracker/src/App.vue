2febfa11876ca15b5acfaa74a07cd25bc1f12314
Bruna Barbieri <bruh_barbieri@hotmail.com>
2024-12-04 22:20:39 -0300

    curso avançando no vue

--- Formação Framework Vue.js 3/alura-tracker/src/App.vue
+++ Formação Framework Vue.js 3/alura-tracker/src/App.vue
@@ -1,62 +1,37 @@
 <template>
-  <main class="columns is-gapless is-multiline" :class="{ 'modo-escuro': modoEscuroAtivo }">
+  <main class="columns is-gapless is-multiline" :class="{ 'modo-escuro' : modoEscuro}">
     <div class="column is-one-quarter">
-      <BarraLateral @aoTemaAlterado="trocarTema"/>
+      <BarraLateral @aoAlterarModo="alterarModo"/>
     </div>
-    <div class="column is-three-quarter conteudo">
-      <FormularioCadastro @aoSalvarTarefa="salvarTarefa"/>
-      <div class="lista">
-        <TarefaFormulario v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa"/>
-        <BoxFormulario v-if="listaEstaVazia">
-          Você não está muito produtivo hoje :(
-        </BoxFormulario>
-      </div>
+    <div class="column is-three-quarters conteudo">
+      <router-view></router-view>
     </div>
   </main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import BarraLateral from './components/BarraLateral.vue'
-import FormularioCadastro from './components/FormularioCadastro.vue'
-import TarefaFormulario from './components/TarefaFormulario.vue'
-import BoxFormulario from './components/BoxFormulario.vue'
-import ITarefa from './interfaces/ITarefa'
+import { defineComponent } from "vue";
+import BarraLateral from "./components/BarraLateral.vue";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
     BarraLateral,
-    FormularioCadastro,
-    TarefaFormulario,
-    BoxFormulario
   },
   data () {
     return {
-      tarefas: [] as ITarefa[],
-      modoEscuroAtivo: false
-    }
-  },
-  computed: {
-    listaEstaVazia () : boolean {
-      return this.tarefas.length === 0
+      modoEscuro: false
     }
   },
   methods: {
-    salvarTarefa (tarefa: ITarefa) {
-      this.tarefas.push(tarefa)
-    },
-    trocarTema (modoEscuroAtivo: boolean) {
-      this.modoEscuroAtivo = modoEscuroAtivo
+    alterarModo (modoEscuro: boolean) : void {
+      this.modoEscuro = modoEscuro
     }
   }
 });
 </script>
 
 <style>
-.lista {
-  padding: 1.25rem;
-}
 main {
   --bg-primario: #fff;
   --texto-primario: #000;
@@ -64,6 +39,9 @@ main {
 main.modo-escuro {
   --bg-primario: #2b2d42;
   --texto-primario: #ddd;
+}
+.lista {
+  padding: 1.25rem;
 }
 .conteudo {
   background-color: var(--bg-primario);
